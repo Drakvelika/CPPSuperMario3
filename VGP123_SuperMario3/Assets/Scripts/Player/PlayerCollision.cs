2cd6dc394e723b4bec87329c8c6f72ddaa4aa709
dr4gon5layer <dr4gon5layer@hotmail.com>
2021-05-28 07:58:08 -0600

    Lab 6 Starter Code

---
+++ VGP123_SuperMario3/Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D Col)
+    {
+        if (Col.gameObject.tag == "EnemyProjectile")
+        {
+            GameManager.instance.lives--;
+            Destroy(Col.gameObject);
+        }
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
